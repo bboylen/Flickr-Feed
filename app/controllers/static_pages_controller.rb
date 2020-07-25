aac56520ce265a6f3b0612f406f79a33d083c3e7
Brian Boylen <bbbrianboylen@gmail.com>
2020-07-25 11:06:00 -0500

    Added photo feed functionality

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
   def home
     if params[:user_id]
-      @photos = Flickr.photos.search(user_id: params[:id])
+      @photos = Flickr.photos.search(user_id: params[:user_id])
     end
   end
 
